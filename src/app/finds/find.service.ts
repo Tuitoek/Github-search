0c8a203e1141f2a1fce997ce35048bb678399fd6
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-23 12:50:34 +0300

    Changed structure

--- src/app/finds/find.service.ts
+++ src/app/finds/find.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import{Users} from '../users';
 import{Repo} from '../repo';
 import {HttpClient} from '@angular/common/http';
-import {environment} from '../../environments/environment'
 
 @Injectable({
   providedIn: 'root'
